858dbeae698f8d6abea581306112e3ecccd25f25
Søren Ditlefsen <soeren_ditlefsen@hotmail.com>
2022-10-11 15:26:08 +0200

    Some basic components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import Dropdown from "./Libs/UI/Dropdown";
+import Input from "./Libs/UI/Input";
+
 function App() {
   return (
-    <div>
-      Hej
-    </div>
+    <>
+      <div> Hvor mange dage er du væk?</div>
+      <Input text="Antal dage"></Input>
+      <div>
+        Hvilken type rejse skal du på?
+      </div>
+      <Dropdown onChange={() => {}}></Dropdown>
+
+      <div>
+        Her er listen.
+      </div>
+    </>
   );
 }
 
