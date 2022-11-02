417e9b609c6cde0c591fb71b7b285878fb1132fb
Søren Ditlefsen <sredit@norlys.dk>
2022-11-02 13:30:59 +0100

    Fixed dropdown

--- src/Libs/UI/Dropdown.tsx
+++ src/Libs/UI/Dropdown.tsx
@@ -1,28 +1,34 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { useState } from "react";
 
 interface Props {
     onChange: () => void;
 }
 
 function Dropdown(props: Props) {
-    let v = 0;
+    const [value, setValue] = useState("");
+
+    const handleChange = (event: SelectChangeEvent<string>) => {
+        setValue(event.target.value);
+    }
 
     return (
         <FormControl fullWidth>
-            <InputLabel id="simple-select-label">Rejsetype</InputLabel>
+            <InputLabel id="simple-select-label">Rejsetyper</InputLabel>
             <Select
                 labelId="simple-select-label"
                 id="simple-select"
-                value={v}
+                value={value}
                 label="TravelType"
-                onChange={() => console.log(v)}
+                onChange={handleChange}
             >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                <MenuItem value={1} >Ten</MenuItem>
+                <MenuItem value={2}>Twenty</MenuItem>
+                <MenuItem value={3}>Thirty</MenuItem>
             </Select>
         </FormControl>
     );
 }
 
 export default Dropdown
+
