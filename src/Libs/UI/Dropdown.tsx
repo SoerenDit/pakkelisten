e2b69e4063f060d1f13c5207f9800cb2d8c6c082
Søren Ditlefsen <sredit@norlys.dk>
2022-11-02 13:47:23 +0100

    Added margins to dropdown and input

--- src/Libs/UI/Dropdown.tsx
+++ src/Libs/UI/Dropdown.tsx
@@ -1,34 +1,46 @@
-import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 import { useState } from "react";
+import styled from "styled-components";
 
 interface Props {
-    onChange: () => void;
+  onChange: () => void;
 }
 
 function Dropdown(props: Props) {
-    const [value, setValue] = useState("");
+  const [value, setValue] = useState("");
 
-    const handleChange = (event: SelectChangeEvent<string>) => {
-        setValue(event.target.value);
-    }
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    setValue(event.target.value);
+  };
 
-    return (
-        <FormControl fullWidth>
-            <InputLabel id="simple-select-label">Rejsetyper</InputLabel>
-            <Select
-                labelId="simple-select-label"
-                id="simple-select"
-                value={value}
-                label="TravelType"
-                onChange={handleChange}
-            >
-                <MenuItem value={1} >Ten</MenuItem>
-                <MenuItem value={2}>Twenty</MenuItem>
-                <MenuItem value={3}>Thirty</MenuItem>
-            </Select>
-        </FormControl>
-    );
+  const Container = styled.div`
+    margin: 10px;
+  `;
+
+  return (
+    <Container>
+      <FormControl fullWidth>
+        <InputLabel id="simple-select-label">Rejsetyper</InputLabel>
+        <Select
+          labelId="simple-select-label"
+          id="simple-select"
+          value={value}
+          label="TravelType"
+          onChange={handleChange}
+        >
+          <MenuItem value={1}>Vandretur</MenuItem>
+          <MenuItem value={2}>Cykeltur</MenuItem>
+          <MenuItem value={3}>Bytur</MenuItem>
+        </Select>
+      </FormControl>
+    </Container>
+  );
 }
 
-export default Dropdown
-
+export default Dropdown;
--- src/Libs/UI/Input.tsx
+++ src/Libs/UI/Input.tsx
@@ -1,14 +1,25 @@
 import { TextField } from "@mui/material";
+import styled from "styled-components";
 
 interface Props {
-    text: string;
+  text: string;
 }
 
+const Container = styled.div`
+  margin: 10px;
+`;
+
 function Input(props: Props) {
-    return (
-        <TextField id="days-away" label={props.text} variant="outlined"></TextField>
-    );
-  }
-  
-  export default Input;
-  
+  return (
+    <Container>
+      <TextField
+        fullWidth
+        id="days-away"
+        label={props.text}
+        variant="outlined"
+      ></TextField>
+    </Container>
+  );
+}
+
+export default Input;
