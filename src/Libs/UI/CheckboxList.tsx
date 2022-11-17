138f8bab389761efec2cc660d6e9350d51537d6e
Søren Ditlefsen <sredit@norlys.dk>
2022-11-17 11:25:46 +0100

    Question mark to the right of each item gives a popup with description

--- src/Libs/UI/CheckboxList.tsx
+++ src/Libs/UI/CheckboxList.tsx
@@ -6,11 +6,13 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
+import ReadMoreIcon from '@mui/icons-material/Help';
+
 import configuration from '../features/configuration.json';
 import items from '../features/items.json';
 import styled from 'styled-components';
-import { Typography } from '@mui/material';
+import { Dialog, DialogContent, DialogContentText, DialogTitle, Typography } from '@mui/material';
+import { useState } from 'react';
 
 
 interface Props {
@@ -18,7 +20,8 @@ interface Props {
 }
 
 export default function CheckboxList(props: Props) {
-  const [checked, setChecked] = React.useState([0]);
+  const [checked, setChecked] = useState([0]);
+  const [openDialog, setOpenDialog] = useState(-1);
 
   const handleToggle = (value: number) => () => {
     const currentIndex = checked.indexOf(value);
@@ -38,6 +41,7 @@ export default function CheckboxList(props: Props) {
    border-radius: 4px;
 `;
 
+
   return (
     <Container>
       <Typography align="center" sx={{padding:"20px 0px 0px 0px", margin:"0px", fontWeight:"bold"}}>
@@ -52,11 +56,19 @@ export default function CheckboxList(props: Props) {
               key={index}
               secondaryAction={
                 <IconButton edge="end" aria-label="comments">
-                  <CommentIcon />
+                  <ReadMoreIcon onClick={() => setOpenDialog(index)}/>
                 </IconButton>
               }
               disablePadding
             >
+              <Dialog open={openDialog === index} onClose={() => setOpenDialog(-1)}>
+                <DialogTitle>{item.name}</DialogTitle>
+                <DialogContent>
+                  <DialogContentText>
+                    {item.description}
+                  </DialogContentText>
+                </DialogContent>
+              </Dialog>
               <ListItemButton role={undefined} onClick={handleToggle(index)} dense>
                 <ListItemIcon>
                   <Checkbox
@@ -73,39 +85,6 @@ export default function CheckboxList(props: Props) {
           );
         })}
       </List>
-      <Typography align="center" sx={{padding:"20px 0px 0px 0px", margin:"0px", fontWeight:"bold"}}>
-        Toilettaske
-      </Typography>
-      <List sx={{ width: '100%', maxWidth: configuration.maxWidth, margin: "0px" }}>
-        {items.toiletries.map((item, index) => {
-          const labelId = `checkbox-list-label-${item.name}`;
-
-          return (
-            <ListItem
-              key={index+7}
-              secondaryAction={
-                <IconButton edge="end" aria-label="comments">
-                  <CommentIcon />
-                </IconButton>
-              }
-              disablePadding
-            >
-              <ListItemButton role={undefined} onClick={handleToggle(index+7)} dense>
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    checked={checked.indexOf(index+7) !== -1}
-                    tabIndex={-1}
-                    disableRipple
-                    inputProps={{ 'aria-labelledby': labelId }}
-                  />
-                </ListItemIcon>
-                <ListItemText id={labelId} primaryTypographyProps={{ fontSize: '16px' }} primary={`${Math.floor(props.days * item.factor + item.additional)} x ${item.name}`} />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-        </List>
     </Container>
   );
 }
